bea85eebd6a65ae2a9d2caa0f7fa3115f887c08b
Amith B V <amithrao0606@gmail.com>
2025-08-15 13:09:57 +0530

    Intro to Templates and static folder.

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 <img src="./public/Pic-1.png" />
 <img src="./public/Pic-2.png" />
+
+### Folder Structure :
+
+```
+static/ - for css and js files
+templates/ - only for html templates
+```
--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello world. You are at chai aur Django Home Page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello world. You are at chai aur Django About Page")
