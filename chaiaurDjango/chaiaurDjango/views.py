92b01a3b4069bec69d3a03eee4cc6547c4b31823
Amith B V <amithrao0606@gmail.com>
2025-08-15 12:25:48 +0530

    First request-response cycle.

---
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello world. You are at chai aur Django Home Page")
+
+def about(request):
+    return HttpResponse("Hello world. You are at chai aur Django About Page")
+
+def contact(request):
+    return HttpResponse("Hello world. You are at chai aur Django Contact Page")
